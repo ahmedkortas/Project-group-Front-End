550d25ff7e07002dbeb0bc913747979f6594f982
Ahmed Kortas <42817639+ahmedkortas@users.noreply.github.com>
2022-06-14 15:45:33 +0200

    Add files via upload
    semi product working need improving

--- src/components/NavComponent.jsx
+++ src/components/NavComponent.jsx
@@ -4,22 +4,22 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 import Container from "react-bootstrap/Container";
 
 function NavComponent(props) {
-	return (
-		<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" id="navbar">
-			<Container>
-				<Navbar.Brand href="#home"></Navbar.Brand>
-				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
-				<Navbar.Collapse id="responsive-navbar-nav">
-					<Nav className="me-auto">
-						<Nav.Link href="#features">Features</Nav.Link>
-						<Nav.Link href="#pricing">Pricing</Nav.Link>
-						<Nav.Link href="#features">Features</Nav.Link>
-						<Nav.Link href="#pricing">Pricing</Nav.Link>
-					</Nav>
-				</Navbar.Collapse>
-			</Container>
-		</Navbar>
-	);
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" id="navbar">
+      <Container>
+        <Navbar.Brand href="#home"></Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Kunde</Nav.Link>
+            <Nav.Link href="#pricing">Kunde</Nav.Link>
+            <Nav.Link href="#features">Kunde</Nav.Link>
+            <Nav.Link href="#pricing">Kunde</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default NavComponent;
---
+++ src/components/VisitsFilterByUser.jsx
@@ -0,0 +1,61 @@
+import axios from "axios";
+import moment from "moment";
+
+function VisitsFilterByUser({ visits }) {
+  let clickHandler = (e) => {
+    e.preventDefault();
+    let input = document.getElementById("visitsFilter").value;
+    let filter = input.split(",");
+    //transfor all elements of the filter array to numbers with for loop
+    for (let i = 0; i < filter.length; i++) {
+      filter[i] = Number(filter[i]);
+    }
+    let obj = visits;
+    //loop over the obj and pop every elements where training_sessions is not included in the filter array
+    let arr = [];
+    for (let i = 0; i < obj.length; i++) {
+      let obj2 = [];
+      if (filter.includes(obj[i].training_sessions)) {
+        obj2[0] = obj[i]["id"];
+        obj2[1] = obj[i]["user_name"];
+        obj2[2] = obj[i]["training_sessions"];
+        obj2[3] = moment(obj[i]["start_time"]).format("HH:mm");
+        // obj2["end_time"] = moment(obj[i]["end_time"]).format("HH:mm");
+
+        obj2[4] = moment(obj[i]["start_time"]).format("DD-MM-YYYY");
+        obj2[5] = moment(obj[i]["start_time"]).diff(obj[i]["end_time"]); // need to be changed to the last end time and switch the diff
+        arr.push(obj2);
+      }
+    }
+    axios({
+      url: "http://127.0.0.1:8000/visits/excel",
+      method: "post",
+      data: arr,
+      responseType: "blob",
+    }).then((res) => {
+      const url = window.URL.createObjectURL(new Blob([res.data]));
+      const link = document.createElement("a");
+      link.href = url;
+      link.setAttribute("download", "file.xls"); //or any other extension
+      document.body.appendChild(link);
+      link.click();
+    });
+    console.table(obj);
+  };
+
+  return (
+    <div className="visitsList">
+      <form onSubmit={clickHandler}>
+        <input
+          id="visitsFilter"
+          type="text"
+          placeholder="enter the training saision please separe with a comma ,"
+          style={{ width: "70%" }}
+        />
+        <input type="submit" placeholder="send" />
+      </form>
+    </div>
+  );
+}
+
+export default VisitsFilterByUser;
